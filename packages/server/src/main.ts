ee3c3c15fe27f89b40ee0c2ae0eec8ac43350335
Jan Litzenburger <jan.litzenburger@sap.com>
2022-01-03 18:01:20 +0100

    enable remote server

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv-flow';
+dotenv.config();
 
 async function bootstrap() {
+  console.log('process.env.CORS_ALLOW_ORIGIN', process.env.CORS_ALLOW_ORIGIN);
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CORS_ALLOW_ORIGIN,
   });
   await app.listen(3001);
 }
